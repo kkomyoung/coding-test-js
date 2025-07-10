78b473d2b54c8a4654a951b128609cba60648750
Young-ae Kim <kya754@gmail.com>
2025-07-10 14:46:51 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,24 +1,14 @@
 function solution(dot) {
-    // 배열의 첫번째 숫자
-    const firstNumber = dot[0];
-    // 배열의 두번째 숫자
-    const secondNumber = dot[1];
+    // 구조분해 할당
+    const [x, y] = dot;
     
     // 두개 다 양수면 1
-    if (firstNumber > 0 && secondNumber > 0) {
-        return 1;
-    } 
+    if (x > 0 && y > 0) return 1;
     // 두개 다 음수면 3
-    else if (firstNumber < 0 && secondNumber < 0) {
-         return 3;
-    }
+    if (x < 0 && y < 0) return 3;
      // 첫번째가 음수이고 두번째가 양수면 2
-    else if (firstNumber < 0 && secondNumber > 0) {
-         return 2;
-    }
+    if (x < 0 && y > 0) return 2;
     // 첫번째가 양수이고 두번쨰가 음수면 4
-    else if (firstNumber > 0 && secondNumber < 0) {
-         return 4;
-    }
+    if (x > 0 && y < 0) return 4;
     
 }
