52cc3cc92788f12b097b7c73d8e76f60cba532f1
Young-ae Kim <kya754@gmail.com>
2025-07-11 11:42:40 +0900

    [level 0] Title: 순서쌍의 개수, Time: 28.91 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let length = 0;
+    
+    for(let i=1; i <= n; i++) {
+        if (n % i === 0) {
+            length+=1;
+        }
+    }
+    
+    return length;
+}
