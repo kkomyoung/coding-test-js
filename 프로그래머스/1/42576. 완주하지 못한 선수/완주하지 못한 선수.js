0b98496940c4fd80c0ad21e35c462a7442b193d6
YoungAe Kim <kya754@gmail.com>
2025-08-08 17:15:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 46.76 ms, Memory: 66.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,27 @@
+function solution(participant, completion) {
+    let result = ''
+    const map = new Map();
+    
+    // participant를 키로 map 만들기
+    for (const name of participant){
+        // 동명이인 이라면?
+        if (map.has(name)) {
+            map.set(name, map.get(name) - 1)
+        } else {
+            map.set(name, 0)
+        }
+    }
+    
+    // 만들어진거랑 완주자랑 비교해서 있으면 값 1 올려주기 값이 0인곳에만 딱 한번 올릴 수 있음
+    for (const name of completion) {
+        map.set(name, map.get(name) + 1)
+    }
+    
+    for (const key of map.keys()){
+        if (map.get(key) === 0) {
+            result = key
+        }
+    }
+    
+    return result;
+}
