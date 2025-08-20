4ace60ee86bbd4523aad85543be0e3b919f536f7
YoungAe Kim <kya754@gmail.com>
2025-08-20 19:29:28 +0900

    [level 2] Title: 올바른 괄호, Time: 32.90 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,30 +1,17 @@
-// 꺼낸게 만약 ) 이거야 그러면 이전꺼를 체크해
-    // 이전께 없어 => false
-
-// 꺼낸게 만약 ( 이거야 그러면 빼서 새로운 배열에 추가해 ["("]
-// 그다음께 또 ( 이거야 그러면 또 빼서 추가해 ["(", "("]
-// 그다음은 ) 이거야 그러면 배열에서 마지막꺼를 제외해 ["("]
-
-// 반복이 끝났을 때 배열이 비어있으면 성공!
-
 function solution(s){
-    // 문자를 배열로 쪼개기
-    const stringArray = [...s]
-    // 임시공간
-    const temp = []
+    const stack = [];
     
-    // 첫번째 요소 체크
-    if (stringArray[0] === ")") {
-        return false
-    }
-    
-    for (const string of stringArray) {
-        if (string === "(") {
-            temp.push("(")
+    for (const ch of s) {
+        if (ch === "(") {
+            stack.push("(");
         } else {
-            temp.pop()
+            if (stack.length === 0) {
+                return false;
+            } else {
+                stack.pop();
+            }
         }
     }
-
-    return temp.length === 0 ? true : false;
+    
+    return stack.length === 0;
 }
